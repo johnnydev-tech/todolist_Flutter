2c912bf73a42bfe5358760e23cbba2707bd84afa
Johnny Freire <johnnyfreirejh@hotmail.com>
2021-02-26 00:18:40 -0300

    Create interface

--- lib/View/Home.dart
+++ lib/View/Home.dart
@@ -6,16 +6,51 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List _listaTarefas = ["Estudar Flutter", "Fazer Curso", "Ler"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("TODO List"),
       ),
-      body: Container(),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                  itemCount: _listaTarefas.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(_listaTarefas[index]),
+                    );
+                  }),
+            ),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.purple,
         child: Icon(Icons.add),
-        onPressed: () {},
+        tooltip: "Nova Tarefa",
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text("Nova Tarefa"),
+                  content: TextField(
+                    decoration: InputDecoration(labelText: "Digite sua Tarefa"),
+                  ),
+                  actions: [
+                    FlatButton(
+                      child: Text("Salvar"),
+                      onPressed: (){},
+                    ),
+                  ],
+                );
+              });
+        },
       ),
     );
   }
