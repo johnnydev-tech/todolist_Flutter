8c548846a7f0d4d69cfbe1f02638212f63dbc0f9
JohnnyFreire97 <johnnyfreirejh@hotmail.com>
2021-06-03 01:39:48 -0300

    feature add and remove task

--- lib/View/Home.dart
+++ lib/View/Home.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,14 +10,113 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List _listaTarefas = [
-    "Estudar Flutter",
-    "Fazer Curso",
-    "Ler",
-  ];
+  TextEditingController _textController = TextEditingController();
+  List _listaTarefas = [];
+  Map<String, dynamic> _lastTask = Map();
+
+  Future<File> _getFile() async {
+    final diretorio = await getApplicationDocumentsDirectory();
+    return File('${diretorio.path}/dados.json');
+  }
+
+  _saveFile() async {
+    var arquivo = await _getFile();
+
+    String dados = json.encode(_listaTarefas);
+    arquivo.writeAsString(dados);
+  }
+
+  _saveTask() {
+    String text = _textController.text;
+
+    Map<String, dynamic> tarefa = Map();
+    tarefa['titulo'] = text;
+    tarefa['status'] = false;
+    setState(() {
+      _listaTarefas.add(tarefa);
+    });
+    _saveFile();
+    _textController.clear();
+  }
+
+  _readFile() async {
+    try {
+      final arquivo = await _getFile();
+      return arquivo.readAsString();
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  _removeTask(index) {
+    _listaTarefas.removeAt(index);
+    _saveFile();
+  }
+
+  Widget itemList(context, index) => Dismissible(
+        direction: DismissDirection.endToStart,
+        key: Key(
+          '${_listaTarefas[index]['titulo']}-${_listaTarefas[index]}-${DateTime.now().millisecondsSinceEpoch}',
+        ),
+        background: Container(
+          color: Colors.red,
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Icon(
+                Icons.delete,
+                color: Colors.white,
+              )
+            ],
+          ),
+        ),
+        onDismissed: (direction) {
+          _lastTask = _listaTarefas[index];
+          //Remove
+          _removeTask(index);
+
+          //snackbar
+          final snackbar = SnackBar(
+            content: Text('Tarefa Removida!!!'),
+            action: SnackBarAction(
+              label: 'Desfazer',
+              onPressed: () {
+                setState(() {
+                  _listaTarefas.insert(index, _lastTask);
+                });
+                _saveFile();
+              },
+            ),
+          );
+          Scaffold.of(context).showSnackBar(snackbar);
+        },
+        child: CheckboxListTile(
+          value: _listaTarefas[index]['status'],
+          title: Text(_listaTarefas[index]['titulo']),
+          onChanged: (value) {
+            setState(() {
+              _listaTarefas[index]['status'] = value;
+            });
+            _saveFile();
+          },
+        ),
+      );
+
+  @override
+  void initState() {
+    super.initState();
+    _readFile().then((datas) {
+      setState(() {
+        _listaTarefas = json.decode(datas);
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    print(_listaTarefas.toString());
     return Scaffold(
       appBar: AppBar(
         title: Text("TODO"),
@@ -23,12 +126,9 @@ class _HomeState extends State<Home> {
           children: [
             Expanded(
               child: ListView.builder(
-                  itemCount: _listaTarefas.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(_listaTarefas[index]),
-                    );
-                  }),
+                itemCount: _listaTarefas.length,
+                itemBuilder: itemList,
+              ),
             ),
           ],
         ),
@@ -44,6 +144,7 @@ class _HomeState extends State<Home> {
                 return AlertDialog(
                   title: Text("Nova Tarefa"),
                   content: TextField(
+                    controller: _textController,
                     decoration: InputDecoration(labelText: "Digite sua Tarefa"),
                     onChanged: (text) {},
                   ),
@@ -54,8 +155,8 @@ class _HomeState extends State<Home> {
                     ),
                     FlatButton(
                       child: Text("Salvar"),
-                      onPressed: () {
-                        //TODO: Salvar
+                      onPressed: () async {
+                        await _saveTask();
                         Navigator.pop(context);
                       },
                     ),
